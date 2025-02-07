327b444f27d90ba6abe7ab24657f0e0b1f187482
Emmy Dunsford <emmydunsford@Emmys-MacBook-Pro-2.local>
2025-02-07 14:04:53 -0500

    connecting the javascript page formatting to the other two

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// Select the button element using a CSS selector
+const button = document.querySelector('button');
+
+// Add an event listener for the click event on the button
+button.addEventListener('click', function() {
+    alert('Button was clicked!');
+});
